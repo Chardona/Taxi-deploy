ee19b2ff5b8e156b4bf713bed7c8f471d8eae531
Олег Белый <belochard@gmail.com>
2022-07-17 23:16:22 +0300

    Added 4 tests

--- taxi/tests/test_views.py
+++ taxi/tests/test_views.py
@@ -3,9 +3,11 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 from taxi.models import Manufacturer
+from taxi_service.settings import AUTH_USER_MODEL
 
 CARS_URL = reverse("taxi:car-list")
 MANUFACTURER_URL = reverse("taxi:manufacturer-list")
+DRIVERS_URL = reverse("taxi:driver-list")
 
 
 class PublicCarTest(TestCase):
@@ -18,6 +20,26 @@ class PublicCarTest(TestCase):
         self.assertNotEqual(res.status_code, 200)
 
 
+class PublicManufacturerTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_login_required(self):
+        res = self.client.get(MANUFACTURER_URL)
+
+        self.assertNotEqual(res.status_code, 200)
+
+
+class PublicDriverTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_login_required(self):
+        res = self.client.get(AUTH_USER_MODEL)
+
+        self.assertNotEqual(res.status_code, 200)
+
+
 class PrivateManufacturerTest(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(
@@ -26,7 +48,7 @@ class PrivateManufacturerTest(TestCase):
         )
         self.client.force_login(self.user)
 
-    def test_login_required(self):
+    def test_manufacturer_login_required(self):
         Manufacturer.objects.create(
             name="BMW",
             country='Germany'
@@ -41,3 +63,9 @@ class PrivateManufacturerTest(TestCase):
 
         self.assertEquals(res.status_code, 200)
         self.assertEquals(list(res.context["manufacturer_list"]), list(manufacturers))
+
+    def test_user_login_required(self):
+        res = self.client.get(DRIVERS_URL)
+
+        self.assertEquals(res.status_code, 200)
+        self.assertEquals(list(res.context["driver_list"]), [self.user])
